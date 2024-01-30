2993516a2b16fdef654714ce146219ad67fd5cc2
2DC <kkts9308@gmail.com>
2024-01-30 23:37:08 +0900

    🧑‍💻 typeorm 많이 쓰이는 기능들 정리

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Role, UserModel } from './entity/user.entity';
-import { Repository } from 'typeorm';
+import { ILike, Repository } from 'typeorm';
 import { ProfileModel } from './entity/profile.entity';
+import { PostModel } from './entity/post.entity';
+import { TagModel } from './entity/tag.entity';
 
 @Controller()
 export class AppController {
@@ -11,21 +13,126 @@ export class AppController {
     private readonly userRepository: Repository<UserModel>,
     @InjectRepository(ProfileModel)
     private readonly profileRepository: Repository<ProfileModel>,
+    @InjectRepository(PostModel)
+    private readonly postRepository: Repository<PostModel>,
+    @InjectRepository(TagModel)
+    private readonly tagRepository: Repository<TagModel>,
   ) {}
 
-  @Get('users')
-  getUser() {
-    return this.userRepository.find({
-      relations: {
-        profile: true,
+  @Get('sample')
+  async getSample() {
+    // craete
+    // 모델에 해당되는 객체 생성 - 저장은 안함 (객체만 생성)
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const user1 = this.userRepository.create({
+      email: '',
+    });
+
+    // save
+    // 모델에 해당되는 객체 생성 - 저장까지 함
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const user2 = await this.userRepository.save({
+      email: '',
+    });
+
+    // preload
+    // 입력된 값을 기반으로 데이터베이스에 있는 데이터를 불러오고
+    // 추가 입력된 값으로 데이터베이스에서 가져온 값들을 대체함.
+    // 저장하지는 않음. (find와 create가 섞인 것과 같음)
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const user3 = await this.userRepository.preload({
+      id: 100,
+      email: 'cchhannggee',
+    });
+
+    // 삭제하기
+    await this.userRepository.delete({
+      id: 101,
+    });
+
+    // 값 증가시키기
+    await this.userRepository.increment(
+      {
+        id: 1,
       },
-      select: {
-        profile: {
-          profileImg: true,
-        },
+      'count',
+      2,
+    );
+
+    // 값 감소시키기
+    await this.userRepository.decrement(
+      {
+        id: 1,
+      },
+      'count',
+      2,
+    );
+
+    // 갯수 카운팅하기
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const count = await this.userRepository.count({
+      where: {
+        email: 'cchhannggee',
       },
     });
+
+    // sum
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const sum = await this.userRepository.sum('count', {
+      email: ILike('%0%'),
+    });
+
+    // average
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const average = await this.userRepository.average('count', {
+      email: ILike('%0%'),
+    });
+
+    // 최소값
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const min = await this.userRepository.minimum('count', {
+      email: ILike('%0%'),
+    });
+
+    // 최대값
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const max = await this.userRepository.maximum('count', {
+      email: ILike('%0%'),
+    });
+
+    // find
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const users = await this.userRepository.find({
+      where: {
+        email: ILike('%0%'),
+      },
+      order: {
+        email: 'ASC',
+      },
+      skip: 0,
+      take: 10,
+    });
+
+    // fineOne
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const user = await this.userRepository.findOne({
+      where: {
+        email: ILike('%0%'),
+      },
+    });
+
+    // 페이지네이션 쓸 때 사용함
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const userAndCount = await this.userRepository.findAndCount({
+      take: 10,
+    });
+    // take 만큼의 갯수에 해당하는 데이터를 가져온 후, 나머지 데이터의 갯수를 가져옴
+
+    return 'sample';
   }
+
+  @Get('users')
+  getUser() {}
 
   @Post('users')
   postUser() {
@@ -47,21 +154,92 @@ export class AppController {
       ...user,
     });
 
-    console.log(data);
     return data;
+  }
+
+  @Delete('users/profile/:id')
+  async deleteUserProfile(@Param('id') id: string) {
+    const deletedData = await this.profileRepository.delete(+id);
+    return deletedData;
   }
 
   @Post('users/profile')
   async createUserAndProfile() {
     const user = await this.userRepository.save({
       email: '1234@naver.com',
+      profile: {
+        profileImg: 'asdf.jpg',
+      },
+    });
+    // 관계 정의에 eager를 쓰면 아래처럼 profileRepository를 따로 쓸 필요가 없다.
+    // await this.profileRepository.save({
+    //   profileImg: 'asdf.jpg',
+    //   user,
+    // });
+
+    return user;
+  }
+
+  @Post('user/post')
+  async createUserAndPost() {
+    const user = await this.userRepository.save({
+      email: 'postuser@gmail.com',
     });
 
-    await this.profileRepository.save({
-      profileImg: 'asdf.jpg',
-      user,
+    await this.postRepository.save({
+      author: user,
+      title: 'post 1',
+    });
+
+    await this.postRepository.save({
+      author: user,
+      title: 'post 2',
     });
 
     return user;
+  }
+
+  @Post('posts/tags')
+  async createPostsTags() {
+    const post1 = await this.postRepository.save({
+      title: 'react lecture',
+    });
+
+    const post2 = await this.postRepository.save({
+      title: 'next lecture',
+    });
+
+    await this.tagRepository.save({
+      name: 'javascript',
+      posts: [post1, post2],
+    });
+    await this.tagRepository.save({
+      name: 'typescript',
+      posts: [post2],
+    });
+    await this.tagRepository.save({
+      name: 'backend',
+      posts: [post2],
+    });
+
+    return true;
+  }
+
+  @Get('posts')
+  getPosts() {
+    return this.postRepository.find({
+      relations: {
+        tags: true,
+      },
+    });
+  }
+
+  @Get('tags')
+  getTags() {
+    return this.tagRepository.find({
+      relations: {
+        posts: true,
+      },
+    });
   }
 }
--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -3,12 +3,14 @@ import {
   CreateDateColumn,
   Entity,
   Generated,
+  OneToMany,
   OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
   VersionColumn,
 } from 'typeorm';
 import { ProfileModel } from './profile.entity';
+import { PostModel } from './post.entity';
 
 export enum Role {
   USER = 'user',
@@ -19,8 +21,7 @@ export enum Role {
 export class UserModel {
   // @PrimaryGeneratedColumn()
   // @PrimaryColumn()
-  // Primary Column은 모든 테이블에서 기본적으로 존재해야한다.
-  // 테이블 안에서 각각의 Row를 구분할 수 있는 칼럼이다.
+  // Primary Column은 테이블에서 각 Row를 구분할 수 있는 칼럼이며, 모든 테이블에서 기본적으로 존재해야한다.
   // uuid : Universally Unique Identifier
   // 12313sdf-455asdf-123gtgtg-13ggththt
   @PrimaryGeneratedColumn()
@@ -30,32 +31,32 @@ export class UserModel {
   email: string;
 
   // @Column({
-  //   // 컬럼 타입 지정
+  //   컬럼 타입 지정
   //   type: 'varchar',
 
-  //   // 컬럼 이름 지정
+  //   컬럼 이름 지정
   //   name: 'title',
 
-  //   // 컬럼 길이 지정 (varchar 등에 사용 가능)
+  //   컬럼 길이 지정 (varchar 등에 사용 가능)
   //   length: 255,
 
-  //   // null 허용 여부
+  //   null 허용 여부
   //   nullable: true,
 
-  //   // 업데이트 가능 여부
-  //   // false면 처음 값 저장할때만 값 지정 가능
-  //   // 이후에는 값 변경 불가능
+  //   업데이트 가능 여부
+  //   false면 처음 값 저장할때만 값 지정 가능
+  //   이후에는 값 변경 불가능
   //   update: true,
 
-  //   // find()를 실행할 때 기본으로 값을 불러올지 정함
-  //   // 기본값은 true
+  //   find()를 실행할 때 기본으로 값을 불러올지 정함
+  //   기본값은 true
   //   select: true,
 
-  //   // 아무것도 입력 안했을 때 컬럼에 기본으로 입력되는 값
+  //   아무것도 입력 안했을 때 컬럼에 기본으로 입력되는 값
   //   default: 'test title!!',
 
-  //   // 칼럼중에서 유일무이한 값이 되어야하는지 체크하는 값
-  //   // 회원가입시 이메일 등에 많이 사용함
+  //   칼럼중에서 유일무이한 값이 되어야하는지 체크하는 값
+  //   회원가입시 이메일 등에 많이 사용함
   //   unique: false,
   // })
   // title: string;
@@ -84,6 +85,33 @@ export class UserModel {
   @Generated('uuid') // 자동으로 생성되는 값, @Column과 함께 사용해야한다.
   additionalId: number;
 
-  @OneToOne(() => ProfileModel, (profile) => profile.user)
+  @OneToOne(() => ProfileModel, (profile) => profile.user, {
+    // find() 실행할때마다 항상 같이 가져올 relation
+    // true를 하면 profile을 자동으로 가져옴 (기본값은 false)
+    eager: true,
+
+    // 저장할때 릴레이션을 한번에 저장 가능
+    // 기본값은 false
+    cascade: true,
+
+    // null이 되도 되는가?
+    // 기본값은 true
+    nullable: true,
+
+    // on~ 무엇을 했을 때
+    // 여기서는 관계가 삭제되었을 때
+    // "no action" -> 아무것도 안함
+    // "cascade" ->참조하는 Row도 삭제
+    // "set null" -> 참조하는 Row에서 참조 id를 null로 변경
+    // "set default" -> 기본 세팅으로 설정 (테이블의 기본 세팅)
+    // "restrict" -> 참조하는 Row가 있으면 삭제 불가능
+    onDelete: 'SET NULL',
+  })
   profile: ProfileModel;
+
+  @OneToMany(() => PostModel, (post) => post.author)
+  posts: PostModel[];
+
+  @Column({ default: 0 })
+  count: number;
 }
