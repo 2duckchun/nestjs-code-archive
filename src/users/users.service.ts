58c5fe960ea3a3d48b4e88223fd24d69ded3882b
2DC <kkts9308@gmail.com>
2024-02-03 12:06:15 +0900

    🗃️ user/userService export 및 auth 관련 user 함수 작성

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserModel } from './entities/users.entity';
 import { Repository } from 'typeorm';
@@ -12,5 +12,41 @@ export class UsersService {
 
   async getAllUsers() {
     return await this.userRepository.find();
+  }
+
+  async getUserByNickname(nickname: string) {
+    return await this.userRepository.exists({
+      where: {
+        nickname: nickname,
+      },
+    });
+  }
+
+  async getUserByEmail(email: string) {
+    return await this.userRepository.findOne({
+      where: {
+        email: email,
+      },
+    });
+  }
+
+  async createUser(user: Pick<UserModel, 'nickname' | 'email' | 'password'>) {
+    const isExistUser = await this.userRepository.exists({
+      where: { nickname: user.nickname },
+    });
+    if (isExistUser) {
+      throw new BadRequestException('이미 존재하는 닉네임입니다.');
+    }
+
+    const isExistEmail = await this.userRepository.exists({
+      where: { email: user.email },
+    });
+    if (isExistEmail) {
+      throw new BadRequestException('이미 존재하는 이메일입니다.');
+    }
+
+    const newUser = this.userRepository.create(user);
+
+    return await this.userRepository.save(newUser);
   }
 }
