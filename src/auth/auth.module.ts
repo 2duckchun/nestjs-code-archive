a9a2eb3e8b71ab2862822bac5df616884c3e4a0b
2DC <kkts9308@gmail.com>
2024-02-03 12:00:57 +0900

    🗃️ auth/signToken 함수 생성

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { SECRET_KEY } from './const/auth.const';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: SECRET_KEY,
+    }),
+    UsersModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserModel } from 'src/users/entities/users.entity';
+import { UsersService } from 'src/users/users.service';
+import { SECRET_KEY } from './const/auth.const';
+// import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) {}
+
+  signToken(user: Pick<UserModel, 'id' | 'email'>, isRefreshToken: boolean) {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken ? 'refresh' : 'access',
+    };
+
+    return this.jwtService.sign(payload, {
+      secret: SECRET_KEY,
+      expiresIn: isRefreshToken ? '3600' : '600',
+    });
+  }
+}
