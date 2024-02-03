e3b9198eb095c9f62d90eeb1a67a30234e5f7880
2DC <kkts9308@gmail.com>
2024-02-03 12:03:33 +0900

    🗃️ auth/authenticateWithEmailAndPassword 함수 생성

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserModel } from 'src/users/entities/users.entity';
 import { UsersService } from 'src/users/users.service';
 import { SECRET_KEY } from './const/auth.const';
-// import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,6 +11,23 @@ export class AuthService {
     private readonly jwtService: JwtService,
     private readonly userService: UsersService,
   ) {}
+
+  async authenticateWithEmailAndPassword(
+    user: Pick<UserModel, 'email' | 'password'>,
+  ) {
+    const existingUser = await this.userService.getUserByEmail(user.email);
+    if (!existingUser) {
+      throw new UnauthorizedException('사용자가 존재하지 않습니다.');
+    }
+
+    const passOk = await bcrypt.compare(user.password, existingUser.password);
+
+    if (!passOk) {
+      throw new UnauthorizedException('비밀번호가 일치하지 않습니다.');
+    }
+
+    return existingUser;
+  }
 
   signToken(user: Pick<UserModel, 'id' | 'email'>, isRefreshToken: boolean) {
     const payload = {
