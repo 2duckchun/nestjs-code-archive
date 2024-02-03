343b7f2cd9f62e7fdc776b60a0134271350cf48a
2DC <kkts9308@gmail.com>
2024-02-03 12:07:45 +0900

    ✨ auth/register/email 엔드포인트 생성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register/email')
+  async registerWithEmail(
+    @Body('email') email: string,
+    @Body('nickname') nickname: string,
+    @Body('password') password: string,
+  ) {
+    return this.authService.registerWithEmail({
+      email,
+      nickname,
+      password,
+    });
+  }
 }
